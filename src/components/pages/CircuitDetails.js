5c3fa3e2f4649251b13012ffc709fc9090280f49
EERSF <152952095+EERSF@users.noreply.github.com>
2024-03-04 23:03:53 +0530

    Update CircuitDetails.js

--- src/components/pages/CircuitDetails.js
+++ src/components/pages/CircuitDetails.js
@@ -6,7 +6,7 @@ function CircuitDetails() {
     <div>
         <Navbar />
       <section className="py-5">
-        <div className="container ">
+        <div className="container">
           <div className="row">
             <div className="col-md-12">
             <div className="line2"></div>
